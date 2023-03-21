9e2362fe8a3efb58c64f7e6d122b18424538b8c4
jadekenneth.github.io <jadekennethdarunday@gmail.com>
2023-03-21 11:15:16 +0800

    chore: ver 20

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { Alert, Badge, Button } from "@chakra-ui/react";
+import React from "react";
+const Number6 = () => {
+  return <Button color={"red"}>Hello world</Button>;
+};
+export { Alert, Button, Badge, Number6 };
